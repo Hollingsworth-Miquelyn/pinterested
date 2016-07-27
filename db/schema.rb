f1f530a846001bd11d2a9d948359fc2172162507
Miquelyn Hollingsworth <miquelyn10@gmail.com>
2016-07-27 20:47:40 +0000

    created pins scaffold edited pins views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727162905) do
+ActiveRecord::Schema.define(version: 20160727193131) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "desctiption"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
